cb7f6c3350c8d701f0b0c15a4773d851e261f738
Josh Fowler & kelly becker <student@epicodus.com>
2014-04-11 15:08:16 -0700

    adds admin assignment functionality for admins only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,47 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    render 'index'
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  def new
+    @user = User.new
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Thanks for signing up!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    if current_user != nil
+      if current_user.admin
+        params.require(:user).permit(:admin, :email, :password, :password_confirmation)
+      else
+        params.require(:user).permit(:email, :password, :password_confirmation)
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Amazon::Application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  root to: 'products#index'
+
   resources :users
   resources :products
   resources :carts
